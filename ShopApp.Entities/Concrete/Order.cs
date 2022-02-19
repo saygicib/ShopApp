4dc266df4ee786c34f55068ed03b2db95b467bc8
Buğrahan <bsaygici@hotmail.com>
2022-02-20 00:46:15 +0300

    added carts,cartitems and checkoutView

--- ShopApp.Entities/Concrete/Order.cs
+++ ShopApp.Entities/Concrete/Order.cs
@@ -8,5 +8,36 @@ namespace ShopApp.Entities.Concrete
 {
     public class Order
     {
+        //
+        public int Id { get; set; }
+        public string OrderNumber { get; set; }
+        public string UserId { get; set; }
+        public EnumOrderStatus Status { get; set; }
+        public EnumPaymentType PaymentTypes { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string OrderNote { get; set; }
+        public string PaymentId { get; set; }
+        public string PaymentToken { get; set; }
+        public string ConversationId { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+    }
+
+    public enum EnumPaymentType
+    {
+        CrediCard =0,
+        EFT =1,
+    }
+
+    public enum EnumOrderStatus
+    {
+        Waiting =0,
+        Unpaid =1,
+        Completed =2,
     }
 }
--- ShopApp.WebUI/Controllers/CartController.cs
+++ ShopApp.WebUI/Controllers/CartController.cs
@@ -56,5 +56,9 @@ namespace ShopApp.WebUI.Controllers
             _cartService.DeleteFromCart(_userManager.GetUserId(User), productId);
             return RedirectToAction("Index");
         }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
     }
 }
